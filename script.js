309f212afe42a0ae417765e0a44d67d49af57db0
Cristiana Monteiro <cristianamelomonteiro@gmail.com>
2021-08-20 01:48:36 +0100

    animated logo

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ instructionbtn.addEventListener("click", () => {
 modalCloseBtn.addEventListener("click", () => {
   instructionsModal.style.display = "none";
 });
+
+const tl = gsap.timeline({
+  repeat: -1,
+  yoyo: true,
+  defaults: { duration: 0.75, ease: "steps(3)" },
+});
+
+tl.from(".header__logo .block_l", {
+  y: -150,
+})
+  .from(".header__logo .block_z", {
+    y: -150,
+  })
+  .from(".header__logo .block_o", {
+    y: -150,
+  })
+  .from(".header__logo .block_s", {
+    y: -150,
+  });
